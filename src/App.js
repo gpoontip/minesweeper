77f12e83290bce2057d9d43d5715ed4c3c94e682
Gary Poon Tip <gtip@ghd.com>
2021-04-19 20:15:31 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const height = 16;
+const width = 16;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [...Array(height)].map((y) =>
+        [...Array(width)].map((x) => {
+          return {
+            id: `cell-${x}-${y}`,
+            x,
+            y,
+            flagged: false,
+            mine: false,
+            revealed: false,
+          };
+        })
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <div className="board">
+        {this.state.grid.map((row, i) => (
+          <div className="row" key={`row-${i}`}>
+            {row.map((cell) => (
+              <div className="cell" key={cell.id}>
+                0
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
